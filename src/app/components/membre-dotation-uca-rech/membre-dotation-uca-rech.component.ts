7d17c7c005e068003e933c5e6de052fa6c6bbde0
akhatar <o.akhatar5927@gmail.com>
2023-06-04 18:39:37 +0100

    MembreDotationUcaRech implementation

---
+++ src/app/components/membre-dotation-uca-rech/membre-dotation-uca-rech.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-membre-dotation-uca-rech',
+  templateUrl: './membre-dotation-uca-rech.component.html',
+  styleUrls: ['./membre-dotation-uca-rech.component.css']
+})
+export class MembreDotationUcaRechComponent {
+
+}
