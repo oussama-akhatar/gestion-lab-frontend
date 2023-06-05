de1d7661aa5cdfc382123ede64951d2e983d66b4
akhatar <o.akhatar5927@gmail.com>
2023-06-05 09:14:40 +0100

    MembreDotationUcaRech implementation

--- src/app/models/membreDotationUcaRech.model.ts
+++ src/app/models/membreDotationUcaRech.model.ts
@@ -2,7 +2,7 @@ import {Membre} from "./membre.model";
 import {DotationUcaRech} from "./dotationUcaRech.model";
 
 export interface MembreDotationUcaRech {
-  id: number,
+  id?: number,
   dotationMembre: string,
   membre: Membre,
   dotationUCARech: DotationUcaRech
--- src/app/services/membre-dotation-uca-rech.service.ts
+++ src/app/services/membre-dotation-uca-rech.service.ts
@@ -3,6 +3,7 @@ import {environment} from "../../environments/environment";
 import {Observable} from "rxjs";
 import {Membre} from "../models/membre.model";
 import {HttpClient} from "@angular/common/http";
+import {MembreDotationUcaRech} from "../models/membreDotationUcaRech.model";
 
 @Injectable({
   providedIn: 'root'
@@ -12,23 +13,23 @@ export class MembreDotationUcaRechService {
   private baseUrl = `${environment.apiBaseUrl}/membreDotationUCARech`;
   constructor(private http: HttpClient) { }
 
-  getAllMembres(): Observable<Membre[]> {
-    return this.http.get<Membre[]>(`${this.baseUrl}/all`);
+  getAllMembreDotationUcaRechs(): Observable<MembreDotationUcaRech[]> {
+    return this.http.get<MembreDotationUcaRech[]>(`${this.baseUrl}/all`);
   }
 
-  addMembre(membre: Membre): Observable<Membre> {
-    return this.http.post<Membre>(`${this.baseUrl}/add`, membre);
+  addMembreDotationUcaRech(membreDotationUcaRech: MembreDotationUcaRech): Observable<MembreDotationUcaRech> {
+    return this.http.post<MembreDotationUcaRech>(`${this.baseUrl}/add`, membreDotationUcaRech);
   }
 
-  getMembreById(id: number): Observable<Membre> {
-    return this.http.get<Membre>(`${this.baseUrl}/find/${id}`);
+  getMembreDotationUcaRechById(id: number): Observable<MembreDotationUcaRech> {
+    return this.http.get<MembreDotationUcaRech>(`${this.baseUrl}/find/${id}`);
   }
 
-  updateMembre(membre: Membre): Observable<Membre> {
-    return this.http.put<Membre>(`${this.baseUrl}/update`, membre);
+  updateMembreDotationUcaRech(membreDotationUcaRech: MembreDotationUcaRech): Observable<MembreDotationUcaRech> {
+    return this.http.put<MembreDotationUcaRech>(`${this.baseUrl}/update`, membreDotationUcaRech);
   }
 
-  deleteMembre(id: number): Observable<any> {
+  deleteMembreDotationUcaRech(id: number): Observable<any> {
     return this.http.delete(`${this.baseUrl}/delete/${id}`);
   }
 
