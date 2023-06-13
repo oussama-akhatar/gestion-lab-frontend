8e32535a17f4eea6568166578994811685edd7c7
akhatar <o.akhatar5927@gmail.com>
2023-06-13 11:27:00 +0100

    Some implementations

---
+++ src/app/components/dashboard-directeur/dashboard-directeur.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-directeur',
+  templateUrl: './dashboard-directeur.component.html',
+  styleUrls: ['./dashboard-directeur.component.css']
+})
+export class DashboardDirecteurComponent {
+
+}
